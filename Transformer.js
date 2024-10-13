fb002874dad40f0e0e9121a9103f18ef9c2ebdd0
Rohit-Gupta-369 <everything108rp@gmail.com>
2024-10-13 15:27:23 +0530

    all file add

---
+++ Transformer.js
@@ -0,0 +1,115 @@
+let hero = document.querySelector(".hero");
+let gall_img = document.querySelector(".img");
+let array_img = [];
+const numberOfImages = 25;
+
+for (let i = 0; i < 7; i++) {
+  array_img.push({ key: i + 1, url: `./assests/circle/tc${i + 1}.jpg` });
+}
+// console.log(array_img.length);
+
+let sliderHTML = `
+  <div class="slider-img w-full h-[420px] absolute " style="--qty:${
+    array_img.length
+  };">
+    ${array_img
+      .map(
+        (item, i) => `
+      <div class="list w-[150px] h-[250px] absolute top-[20%] left-[calc(50%-100px)] "  style="--pos:${
+        i + 1
+      };">
+        <img src="${
+          item.url
+        }" class="w-full h-full object-cover flex rounded-lg" alt="${
+          "image - " + (i + 1)
+        }">
+      </div>
+    `
+      )
+      .join("")}
+  </div>
+`;
+hero.innerHTML = sliderHTML;
+
+//galery
+
+function generateRandomNumbers() {
+  const numbers = [];
+  while (numbers.length < numberOfImages) {
+    let randomNum = Math.floor(Math.random() * numberOfImages) + 1;
+    if (!numbers.includes(randomNum)) {
+      numbers.push(randomNum);
+    }
+  }
+  return numbers;
+}
+
+let numm = generateRandomNumbers();
+
+let gallery_img = [{ key: 1, url: "./assests/transformer/t2.jpg" }];
+for (let i = 0; i < numberOfImages; i++) {
+  gallery_img.push({
+    key: i + 1,
+    url: `./assests/transformer/t${numm[i]}.jpg`,
+  });
+}
+let galleryHTML = `${gallery_img
+  .map(
+    (item, i) => `
+
+    <button onclick="showdis(${i + 1})">
+    <div class="relative">
+    <div class="overflow-hidden py-2 ">
+      <img src="${item.url}" alt="image-${
+      i + 1
+    }" class="w-full h-full object-cover flex rounded-xl">
+    </div>
+    <!--<div class="absolute bottom-10 right-[50%]">
+    <h2>Pokemon</h2>
+    </div>-->
+    </div>
+    </button>
+    `
+  )
+  .join("")}`;
+
+gall_img.innerHTML = galleryHTML;
+
+const dis = [];
+for (let i = 0; i < numberOfImages; i++) {
+  dis.push({
+    title: `Pokemon-${i + 1}`,
+    dis: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Animi tempora, unde in eos dolor suscipit est beatae, nobis error quae odit fugit, amet magni ducimus? Laboriosam aliquid dolorum repellendus nostrum fugiat asperiores, ducimus facere iste consequatur esse obcaecati nulla enim delectus architecto pariatur quasi quas.",
+  });
+}
+
+// const locations = document.querySelector(".gallery-img");
+// let numx = 0;
+// let yy = 0;
+// locations.addEventListener("click", (e) => {
+//   numx = e.x;
+//   yy = e.y;
+
+// });
+// function showdis(i) {
+//   document
+//     .getElementsByClassName("img-description")
+//     .item(0)
+//     .classList.remove("hide");
+
+//   document.getElementsByClassName("img").item(0).classList.add("active");
+
+//   document.getElementById("title").textContent = dis[i].title;
+//   document.getElementById("description").textContent = dis[i].dis;
+// }
+// function hidedis() {
+//   document
+//     .getElementsByClassName("img-description")
+//     .item(0)
+//     .classList.add("hide");
+
+//   document.getElementsByClassName("img").item(0).classList.remove("active");
+
+//   document.getElementById("title").textContent = dis.title;
+//   document.getElementById("description").textContent = dis.dis;
+// }
---
+++ main.js
@@ -0,0 +1,86 @@
+
+let tabs = document.getElementById("tabs");
+let tabs_array = ["Pokemon","Marvel","Transformer","Anime",]
+let rannum  = []
+let imageIcon = ["image-line","image-2-line","image-fill","image-2-fill"]
+let icon_image = [""];
+while(rannum.length < imageIcon.length){
+    let ranNum = Math.floor(Math.random() * 4 + 1);
+    if(!rannum.includes(ranNum)){
+        rannum.push(ranNum);
+    }
+
+}
+
+tabs_array.map((ele,i)=>{
+  let hello = `target ="_blank"`;
+  
+    tabs.innerHTML += `
+<a href="./${ele}.html" >
+    <ul class="flex items-center p-2 pl-10  hover:bg-[#82d7ff43] transition-all rounded-lg transition-all gap-2 ">
+        <li>
+           <!-- <i class="ri-${imageIcon[rannum[i] - 1]} text-2xl"></i> -->
+            <img src="./assests/Png/icon-${i + 1}.png" class="w-[25px]" alt="">
+            
+        </li>
+        <li>
+            <h2 class="text-1xl font-medium">${ele}</h2>
+        </li>
+    </ul>
+</a>
+`;
+
+})
+
+
+
+let navbar = document.getElementById("navbar");
+let hideNavElements = document.querySelectorAll(".hidde"); 
+let navArrow = document.getElementById("navbarArrow");
+let flg = 0;
+
+function toggleNavbar() {
+  if (flg) {
+    navbar.classList.add("activeNav");
+    flg = 0;
+  } else {
+    navbar.classList.remove("activeNav");
+    flg = 1;
+  }
+}
+
+hideNavElements.forEach((element) => {
+  element.addEventListener("click", toggleNavbar);
+});
+
+let Images_Up = document.querySelector(".images-up");
+let arrow = document.getElementById("arrow-1");
+let flag = true;
+Images_Up.addEventListener("click", () => {
+
+  if (flag) {
+    arrow.classList.remove("ri-arrow-down-s-line");
+    arrow.classList.add("ri-arrow-up-s-line");
+    flag = false;
+  } else {
+    arrow.classList.remove("ri-arrow-up-s-line");
+    arrow.classList.add("ri-arrow-down-s-line");
+    flag = true;
+  }
+});
+
+let count = 0 ;
+setInterval(()=>{
+  if (count < tabs_array.length) {
+    document.getElementById("img_name").innerHTML = `${tabs_array[count]} `;
+    count += 1;
+  } else {
+    count = 0;
+  }
+
+}, 1500);
+
+
+  let hi = document.getElementById("tabs").id === "tabs";
+  console.log(852);
+  console.log(hi);
